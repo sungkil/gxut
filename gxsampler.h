367c1ce3f7f998c3c8729928fc4e35a02293b555
Sungkil Lee <sungkil@skku.edu>
2021-10-08 17:46:54 +0900

    move poisson to gxsampler.h

--- gxsampler.h
+++ gxsampler.h
@@ -18,8 +18,10 @@
 #ifndef __GX_SAMPLER_H__
 #define __GX_SAMPLER_H__
 
-#if defined(__has_include) && __has_include("gxmath.h")
-	#include "gxmath.h" // the only necessary header for gxmesh
+#ifdef __has_include
+	#if __has_include("gxmath.h")
+		#include "gxmath.h"
+	#endif
 #endif
 
 //*************************************
@@ -113,12 +115,19 @@ inline void tsampler_t<max_samples>::reshape( surface_t dst )
 	}
 }
 
+// forward declarations
+uint hammersley_square(vec4*,uint);
+uint halton_cube(vec4*,uint);
+uint simple_square(vec4*,uint);
+uint poisson_disk( vec4* v, uint _count, bool circular, uint seed );
+
 template <size_t max_samples>
 __noinline void tsampler_t<max_samples>::generate( vec4* v, uint n )
 {
-	if(model==sampler_t::HAMMERSLEY){	uint hammersley_square(vec4*,uint); hammersley_square(v,n); }
-	else if(model==sampler_t::HALTON){	uint halton_cube(vec4*,uint);		halton_cube(v,n); }
-	else {								uint simple_square(vec4*,uint);		simple_square(v,n); }
+	if(model==HAMMERSLEY)	hammersley_square(v,n);
+	else if(model==HALTON)	halton_cube(v,n);
+	else if(model==POISSON)	poisson_disk(v,n,surface==CIRCLE,seed);
+	else					simple_square(v,n);
 }
 
 template <size_t max_samples>
@@ -139,7 +148,7 @@ __noinline uint tsampler_t<max_samples>::resample()
 }
 
 //*************************************
-// simple implementations
+// late implementations
 __noinline uint simple_square( vec4* v, uint n ){ for(uint k=0;k<n;k++,v++) v->xyz=vec3{prand2()*2.0f-1.0f,0.0f}; return n; }
 __noinline uint hammersley_square( vec4* v, uint n ){ const float nf=1.0f/float(n); for(uint k=0;k<n;k++,v++) v->xy=vec2(k*nf,float(bitswap(k)>>8)/float(1<<24))*2.0f-1.0f; return n; } // with radical inverse
 __noinline uint halton_cube( vec4* v, uint n )
@@ -154,6 +163,154 @@ __noinline uint halton_cube( vec4* v, uint n )
 		v->xyz = vec3(x,y,z)*2.0f-1.0f;
 	}
 	return n;
+}
+
+struct bridson_t
+{
+	struct cell {	vec2 pos; bool occupied; bool empty(){ return !occupied; } void clear(){ occupied=false; } };
+	struct sample { vec2 pos; ivec2	tc; int index( int grid_size ){ return grid_size*tc.y+tc.x; } };
+
+	const bool			circular;
+	float				r;
+	int					grid_size;
+	std::vector<cell>	grid;
+	std::vector<vec2>	samples;
+	std::deque<sample>	active_queue;
+	
+	bridson_t( bool circ ):circular(circ){};
+	void reset( const int count, float radius ){ r=radius; grid_size=int(ceil(1.0f/cell_size())); grid.resize(grid_size*grid_size); for(auto& c:grid) c.clear(); active_queue.clear(); samples.clear(); samples.reserve(count*3); } /* 3 times more samples */
+	float cell_size(){ return r/1.4142135623730950488f; }
+	bool in_bound( vec2 v ){ return (!circular&&v.x>=0&&v.x<1.0f&&v.y>=0&&v.y<1.0f)||(circular&&((v.x-0.5f)*(v.x-0.5f)+(v.y-0.5f)*(v.y-0.5f))<0.25f); }
+	bool in_grid( ivec2 v ){ return v.x>=0&&v.x<grid_size&&v.y>=0&&v.y<grid_size; }
+	ivec2 to_grid( vec2& v ){ return ivec2(int((v.x/cell_size())), int((v.y/cell_size()))); }
+	sample gen_intial_sample(){ sample s; s.pos=vec2(0.5f,0.5f); s.tc = to_grid(s.pos); return s; }
+	sample gen_annulus( vec2 p ){ sample s; float t = prand()*PI<float>*2.0f; s.pos = p + vec2(cosf(t),sinf(t))*(prand()+1.0f)*r; s.tc = to_grid(s.pos); return s; } 
+	static std::array<ivec2,20> gen_neighbors(){ std::array<ivec2,20> n; for(int k=0,y=-2;y<3;y++)for(int x=-2;x<3;x++){ if((x==0&&y==0)||(abs(x)==2&&abs(y)==2)) continue; n[k++]=ivec2(x,y); } return n; }
+
+	int generate( const int count, float radius );
+};
+
+__noinline path poisson_disk_cache_path( uint count, bool circular, uint seed )
+{
+	static path cache_dir = path::temp(false)+L"global\\sampler\\poisson\\";
+	static uint hash0 = tcrc32<>(__TIMESTAMP__,sizeof(char)*strlen(__TIMESTAMP__));
+	uint hash = hash0;
+	hash = tcrc32<>(&count, sizeof(count), hash );
+	hash = tcrc32<>(&circular, sizeof(circular), hash );
+	hash = tcrc32<>(&seed, sizeof(seed), hash );
+	return cache_dir+format(L"%08x",hash);
+}
+
+__noinline bool poisson_disk_cache( vec4* v, uint count, bool circular, uint seed )
+{
+	path cache_path = poisson_disk_cache_path(count,circular,seed); if(!cache_path.exists()) return false;
+	if(cache_path.file_size()!=sizeof(vec4)*count) return false;
+	FILE* fp = _wfopen( cache_path.c_str(), L"rb" ); if(!fp) return false;
+	size_t read_count = fread( v, sizeof(vec4), count, fp );
+	fclose(fp);
+	return read_count==count;
+}
+
+__noinline void poisson_disk_save( vec4* v, uint count, bool circular, uint seed )
+{
+	path cache_path = poisson_disk_cache_path(count,circular,seed);
+	if(!cache_path.dir().exists()) cache_path.dir().mkdir();
+	FILE* fp = _wfopen( cache_path.c_str(), L"wb" ); if(!fp){ printf("unable to open %s\n",cache_path.wtoa()); return; }
+	fwrite( v, sizeof(vec4), count, fp );
+	fclose(fp);
+}
+
+__noinline int bridson_t::generate( const int count, float radius )
+{
+	// 0. reset all
+	reset(count,radius);
+	static const std::array<ivec2,20> neighbors = gen_neighbors();
+		
+	// 1. generate first sample
+	const float r2 = r*r;
+	sample s0 = std::move(gen_intial_sample());
+	active_queue.emplace_back(s0);
+	samples.emplace_back(s0.pos);
+	grid[s0.index(grid_size)] = {s0.pos,true};
+
+	// 3. process
+	int i=0,in=count*2-1; for(i=0; !active_queue.empty()&&i<in; i++ )
+	{
+		int j = urand()%active_queue.size();
+		sample p = active_queue[j];
+			
+		static const int kn=30;
+		int k; for(k=0;k<kn;k++)
+		{
+			// generate a new sample q around p with [r,2r]
+			sample q = std::move(gen_annulus(p.pos));
+			if(!in_bound(q.pos)) continue;				// out-of-bound sample
+			if(!grid[q.index(grid_size)].empty()) continue;	// already occupied
+
+			// test against neighbor cells
+			bool bfar = true;
+			for(size_t x=0, xn=neighbors.size(); x<xn; x++)
+			{
+				ivec2 n=q.tc+neighbors[x]; if(!in_grid(n)) continue;
+				cell& sn = grid[n.y*grid_size+n.x]; if(sn.empty()) continue;
+				if((q.pos-sn.pos).length2()>r2) continue;
+				bfar = false; break;
+			}
+
+			if(!bfar) continue;
+
+			active_queue.emplace_back(q);
+			samples.emplace_back(q.pos);
+			grid[q.index(grid_size)] = {q.pos,true};
+		}
+
+		if(k==kn) active_queue.erase(active_queue.begin()+j);
+	}
+		
+	return int(samples.size());
+}
+
+__noinline uint poisson_disk( vec4* v, uint _count, bool circular, uint seed )
+{
+	if(poisson_disk_cache(v,_count,circular,seed)) return _count;
+
+	int			count = _count;
+	bridson_t	b(circular);
+	float r0	= sqrtf((circular?(2.0f/5.0f):(8.0f/(5.0f*PI<float>))))/sqrtf(float(count));
+	vec3 r		= vec3(2.0f,0.1f,0.0f)*r0;
+	ivec3 n		= ivec3( b.generate(count,r.x), b.generate(count,r.y), 0 );
+
+	// valid bound and re-construnction
+	while(n.y<count) n.y=b.generate(count,r.y*=0.5f);
+	while(n.x>count) n.x=b.generate(count,r.x*=2.0f);
+
+	// bisection for faster convergence
+	uint k, kn=max(64,int(count*0.2f)); for(k=0; k<kn; k++)
+	{
+		bool conv = fabs(r.y-r.x)<0.00001f;
+		float f = float(count-n.x)/(n.y-n.x);			// center fraction
+		float jitter = conv?(prand()*2.0f-1.0f):0.0f;	// jittering for annealing
+		r.z = max(0.000001f,lerp(r.x,r.y,f+jitter));
+		n.z = b.generate(count,r.z);
+
+		if(n.z==count) break;
+		if(conv) // do not update r anymore
+		{
+			if(n.z>count&&n.z<count+max(2,count/512)) break; // discard 1--2 more samples (for faster convergence)
+		}
+		else if(n.z<count){ n.x=n.z; r.x=r.z; }
+		else if(n.z>count){ n.y=n.z; r.y=r.z; }
+	}
+
+	if(k==kn||n.y<count) printf( "[Sampler] (%d): undersampling (last=%d) in [%d,%d])\n", count, n.z, n.x, n.y );
+
+	// now copy the result
+	for(k=0,kn=min(count,int(b.samples.size()));k<kn;k++) v[k].xy=b.samples[k]*2.0f-1.0f;
+
+	// save cache
+	if(count>=int(_count)) poisson_disk_save( v, _count, circular, seed );
+
+	return count;
 }
 
 #endif // __GX_SAMPLER_H__
