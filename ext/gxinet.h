e6f3be741bd6bb7b23c88a2fea7134ac70e307f0
Sungkil Lee <sungkil@skku.edu>
2021-11-05 11:18:37 +0900

    download return true only when downloaded actually

--- ext/gxinet.h
+++ ext/gxinet.h
@@ -33,8 +33,6 @@ struct file_t
 	path			tmp, dst;	// temporary and final target path; sequetially download and copy
 	size_t			file_size=0;
 	FILETIME		mfiletime={};
-	SYSTEMTIME		msystemtime={};
-	struct { bool	downloaded=false; } b;
 
 	virtual ~file_t(){ release(); }
 	void release(){ if(!hfile) return; InternetCloseHandle(hfile); hfile=nullptr; }
@@ -44,7 +42,6 @@ struct file_t
 
 struct session_t
 {
-	std::vector<std::future<bool>> threads;
 	HINTERNET handle = nullptr;
 
 	virtual ~session_t(){ release(); }
@@ -52,8 +49,11 @@ struct session_t
 	void release(){ if(!handle) return; InternetCloseHandle(handle); handle=nullptr; }
 	operator bool() const { return handle!=nullptr; }
 
-	bool download( std::wstring url, path dst, bool b_wait=true ){ return download(std::vector<std::wstring>{url},dst,b_wait); }
-	bool download( std::vector<std::wstring> urls, path dst, bool b_wait=true );
+	inline bool download( std::wstring url, path dst ){ return download(std::vector<std::wstring>{url},dst); }
+	bool download( std::vector<std::wstring> urls, path dst );
+
+protected:
+	bool download_thread_func( std::vector<std::wstring> urls, path dst );
 };
 
 __noinline bool file_t::open( HINTERNET session, const wchar_t* url )
@@ -63,7 +63,7 @@ __noinline bool file_t::open( HINTERNET session, const wchar_t* url )
 	static const DWORD flags = INTERNET_FLAG_TRANSFER_BINARY|INTERNET_FLAG_RELOAD|INTERNET_FLAG_NO_CACHE_WRITE|INTERNET_FLAG_EXISTING_CONNECT; // INTERNET_FLAG_PRAGMA_NOCACHE
 	release(); hfile=InternetOpenUrlW( session, url, 0, 0, flags, 0); if(!hfile){ release(); return false; }
 	//	DWORD status, dw_size=sizeof(status); if(!HttpQueryInfoW(hfile,HTTP_QUERY_STATUS_CODE|HTTP_QUERY_FLAG_NUMBER,&status,&dw_size,NULL)||status!=HTTP_STATUS_OK){ fprintf( stdout, "%s(%s): https status != HTTP_STATUS_OK\n", __func__, wtoa(url) ); release(); return false; }
-	DWORD dw_size=sizeof(msystemtime); if(!HttpQueryInfoW(hfile,HTTP_QUERY_LAST_MODIFIED|HTTP_QUERY_FLAG_SYSTEMTIME,&msystemtime,&dw_size,NULL)){ release(); return false; }
+	SYSTEMTIME msystemtime={}; DWORD dw_size=sizeof(msystemtime); if(!HttpQueryInfoW(hfile,HTTP_QUERY_LAST_MODIFIED|HTTP_QUERY_FLAG_SYSTEMTIME,&msystemtime,&dw_size,NULL)){ release(); return false; }
 
 	// preprocessing: invalidate second, because HTTP_QUERY_LAST_MODIFIED returns varying second
 	msystemtime.wMilliseconds=0; msystemtime.wSecond=0; // discard seconds and ms
@@ -81,38 +81,30 @@ __noinline bool file_t::get_file_size( HINTERNET session )
 	return true;
 }
 
-__noinline bool download_thread_func( session_t* session, std::vector<std::wstring> urls, path dst )
+__noinline bool session_t::download_thread_func( std::vector<std::wstring> urls, path dst )
 {
-	if(!session||!*session) return false;
+	if(!handle) return false;
 
 	// fetch timestamp of the existing file
-	FILETIME f0={};
-	bool dst_exists = dst.exists();
-	if(dst_exists)
-	{
-		SYSTEMTIME s0=dst.msystemtime(); s0.wMilliseconds=0; s0.wSecond=5; // set small offset forward for local old file
-		SystemTimeToFileTime(&s0,&f0);
-		//fprintf( stdout, "old: %04d-%02d-%02d %02d:%02d:%02d %04d\n", s0.wYear, s0.wMonth, s0.wDay, s0.wHour, s0.wMinute, s0.wSecond, s0.wMilliseconds );
-		//fprintf( stdout, "new: %04d-%02d-%02d %02d:%02d:%02d %04d\t", s1.wYear, s1.wMonth, s1.wDay, s1.wHour, s1.wMinute, s1.wSecond, s1.wMilliseconds );
-	}
+	bool b_dst_exists = dst.exists();
+	FILETIME f0={}; if(b_dst_exists) f0=dst.mfiletime();
 	
 	for( auto url : urls )
 	{
-		file_t f; if(!f.open(session->handle,url.c_str())) continue; // url not exists
-		if(dst_exists&&f.mfiletime<f0){ f.release(); break; } // urls exists, but older
-		
-		if(!f.get_file_size(session->handle)){ f.release(); break; } // now try to get the file size
+		file_t f; if(!f.open(handle,url.c_str())) continue; // url not exists
+		if(b_dst_exists&&!FileTimeGreater(f.mfiletime,f0)) return false; // older url file exists
+		if(!f.get_file_size(handle)){ fprintf(stdout,"error: unable to get file size %s\n", dst.name().wtoa() );return false;} // now try to get the file size
 		std::vector<char> buffer(f.file_size);
 		
-		dst.dir().mkdir();
-		FILE* fp = _wfopen( dst.c_str(), L"wb" ); if(!fp){ fprintf(stdout,"%s(): unable to open %s\n",__func__,dst.wtoa()); return false; }
-		DWORD dwSize, dwRead; do
+		if(!dst.dir().exists()) dst.dir().mkdir();
+		FILE* fp = _wfopen( dst.c_str(), L"wb" ); if(!fp){ fprintf(stdout,"error: unable to open %s\n", dst.name().wtoa());return false;}
+		DWORD dw_size, dw_read; do
 		{
-			InternetQueryDataAvailable(f.hfile, &dwSize, 0, 0);
-			if(dwSize>buffer.size()) dwSize = DWORD(buffer.size());
-			InternetReadFile(f.hfile, buffer.data(), dwSize, &dwRead);
-			if(dwRead) fwrite( buffer.data(), 1, dwRead, fp );
-		} while(dwRead);
+			InternetQueryDataAvailable(f.hfile, &dw_size, 0, 0);
+			if(dw_size>buffer.size()) dw_size = DWORD(buffer.size());
+			InternetReadFile(f.hfile, buffer.data(), dw_size, &dw_read);
+			if(dw_read) fwrite( buffer.data(), 1, dw_read, fp );
+		} while(dw_read);
 		fclose(fp);
 
 		// modify the time stamp
@@ -125,11 +117,10 @@ __noinline bool download_thread_func( session_t* session, std::vector<std::wstri
 	return false;
 }
 
-__noinline bool session_t::download( std::vector<std::wstring> urls, path dst, bool b_wait )
+__noinline bool session_t::download( std::vector<std::wstring> urls, path dst )
 {
 	if(is_offline()||!handle||urls.empty()||dst.empty()) return false;
-	if(!b_wait){ if(threads.empty()) threads.reserve(4096); threads.emplace_back(std::move(std::async(download_thread_func,this,urls,dst))); return false; }
-	auto t=std::async(download_thread_func,this,urls,dst);
+	auto t=std::async(std::launch::async,&session_t::download_thread_func,this,urls,dst);
 	while(std::future_status::ready!=t.wait_for(std::chrono::milliseconds(1)));
 	return t.get();
 }
