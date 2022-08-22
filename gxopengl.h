2e82c6ef86a5dd43470b08b32bbdcdb9edc1befc
slee <sungkil@skku.edu>
2022-08-22 18:59:06 +0900

    update copyright due

--- gxopengl.h
+++ gxopengl.h
@@ -1,5 +1,5 @@
 //*******************************************************************
-// Copyright 2011-2022 Sungkil Lee
+// Copyright 2011-2040 Sungkil Lee
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
