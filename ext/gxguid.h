a7c8910a20e5949c1cd02fd59b84123756a7b498
slee <sungkil@g.skku.edu>
2025-03-23 18:05:02 +0900

    make guid_t restrictive on string

--- ext/gxguid.h
+++ ext/gxguid.h
@@ -27,25 +27,27 @@
 // GUID conversion
 struct guid_t : public GUID
 {
-	guid_t() noexcept { memset(this,0,sizeof(GUID)); }
+	guid_t() noexcept { clear(); }
 	guid_t( const GUID& other ) noexcept { operator=(other); }
 	guid_t( GUID&& other ) noexcept { operator=(other); }
-	guid_t( const char* other ) noexcept { operator=(other); }
-	explicit guid_t( const string& other ) noexcept { operator=(other.c_str()); }
+	guid_t( const char other[39] ) noexcept { operator=(other); }
 
-	guid_t& operator=( const GUID& other ) noexcept { memcpy(this,&other,sizeof(GUID)); return *this; }
-	guid_t& operator=( GUID&& other ) noexcept { auto t=*this; memcpy(this,&other,sizeof(GUID)); memcpy(&other,&t,sizeof(GUID)); return *this; }
-	guid_t& operator=( const char* other ) noexcept { if(!other){ printf( "guid_t::operator=(nullptr)\n" ); return *this; } if(strlen(other)!=38){ printf( "guid_t::operator=(%s): not a guid string\n", other ); return *this; } auto* d=Data4; sscanf_s( other, "{%08lX-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}", &Data1,&Data2,&Data3,d,d+1,d+2,d+3,d+4,d+5,d+6,d+7 ); return *this; }
-	guid_t& operator=( const string& other ) noexcept { return operator=(other.c_str()); }
 	bool operator==( const GUID& other ) const noexcept { return memcmp(this,&other,sizeof(GUID))==0; }
 	bool operator!=( const GUID& other ) const noexcept { return memcmp(this,&other,sizeof(GUID))!=0; }
 
+	guid_t& operator=( const GUID& other ) noexcept { memcpy(this,&other,sizeof(GUID)); return *this; }
+	guid_t& operator=( GUID&& other ) noexcept { auto t=*this; memcpy(this,&other,sizeof(GUID)); memcpy(&other,&t,sizeof(GUID)); return *this; }
+	guid_t& operator=( const char other[39] ) noexcept { auto* h=__to_hex(other); if(!h||!*h||!is_guid(other,h)){printf("guid_t(%s): not a guid\n",other); return clear(); } auto* d=Data4; int read_fields=sscanf_s(h,"%08lX%04hX%04hX%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX",&Data1,&Data2,&Data3,d,d+1,d+2,d+3,d+4,d+5,d+6,d+7); if(read_fields!=11) return clear(); return *this; }
+
 	operator GUID (){ return *this; }
 	operator const GUID () const { return *this; }
-	operator const string () const { return c_str(); }
 	operator bool() const { static decltype(Data4) z={}; return Data1||Data2||Data3||memcmp(Data4,&z,sizeof(z))!=0; }
-	const char* c_str() const { char* b=__strbuf(64); auto* d=Data4; snprintf( b, 64, "{%08lX-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}", Data1,Data2,Data3,d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7] ); return b; }
-	void clear(){ memset(this,0,sizeof(GUID)); }
+	const char* c_str() const { char* b=__strbuf(40); auto* d=Data4; snprintf( b, 64, "{%08lX-%04hX-%04hX-%02hhX%02hhX-%02hhX%02hhX%02hhX%02hhX%02hhX%02hhX}",Data1,Data2,Data3,d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7] ); return b; }
+	guid_t& clear(){ memset(this,0,sizeof(GUID)); return *this; }
+
+	static bool is_guid( const char* s, const char* hex=nullptr ){ return s&&strlen(hex?hex:__to_hex(s))==32; }
+protected:
+	static const char* __to_hex( const char* s ){ if(!s) return nullptr; auto* h = __strbuf(32); size_t j=0; for(size_t k=0,kn=strlen(s);k<kn;k++,s++) if(*s&&isalnum(*s)) h[j++]=*s; return j==32?h:""; }
 };
 
 #endif // __GX_GUID_H__
