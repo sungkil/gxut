031690ce4c27c7d101714ca0fc370693c87b2ab3
slee <sungkil@g.skku.edu>
2025-09-25 14:27:16 +0900

    add sigmoid

--- gxmath.h
+++ gxmath.h
@@ -692,6 +692,10 @@ template <class T,class N,class X> T clamp( T v, N vmin, X vmax ){ return v<T(vm
 template <class T> __forceinline T distance( const tvec2<T>& a, const tvec2<T>& b ){ return (a-b).length(); }
 template <class T> __forceinline T distance( const tvec3<T>& a, const tvec3<T>& b ){ return (a-b).length(); }
 template <class T> __forceinline T distance( const tvec4<T>& a, const tvec4<T>& b ){ return (a-b).length(); }
+__forceinline float sigmoid( float x ){ return 1.0f/(1.0f+exp(-x)); }
+__forceinline vec2 sigmoid( vec2 v ){ return {sigmoid(v.x),sigmoid(v.y)}; }
+__forceinline vec3 sigmoid( vec3 v ){ return {sigmoid(v.x),sigmoid(v.y),sigmoid(v.z)}; }
+__forceinline vec4 sigmoid( vec4 v ){ return {sigmoid(v.x),sigmoid(v.y),sigmoid(v.z),sigmoid(v.w)}; }
 __forceinline vec2 exp( vec2 v ){ return vec2(exp(v.x),exp(v.y)); }
 __forceinline vec3 exp( vec3 v ){ return vec3(exp(v.x),exp(v.y),exp(v.z)); }
 __forceinline vec4 exp( vec4 v ){ return vec4(exp(v.x),exp(v.y),exp(v.z),exp(v.w)); }
